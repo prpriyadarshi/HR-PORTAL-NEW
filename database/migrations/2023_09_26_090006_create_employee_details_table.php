6513d2a3d549c090a59ec9afe38a7bd7dc46f8af
sssreddys <saragada566@gmail.com>
2023-09-27 00:22:05 +0530

    login design

--- database/migrations/2023_09_26_090006_create_employee_details_table.php
+++ database/migrations/2023_09_26_090006_create_employee_details_table.php
@@ -19,7 +19,13 @@ return new class extends Migration
                 $table->string('last_name');
                 $table->date('date_of_birth');
                 $table->string('gender');
+                $table->string('father_name');
+                $table->string('mother_name');
+                $table->date('parent_dob');
+                $table->string('parent_gender');
                 $table->string('email')->unique();
+                $table->string('company_email')->unique();
+                $table->string('company_name');
                 $table->string('mobile_number')->unique();
                 $table->string('alternate_mobile_number')->unique();
                 $table->string('address');
@@ -32,6 +38,7 @@ return new class extends Migration
                 $table->string('department');
                 $table->string('job_title');
                 $table->string('manager_id');
+                $table->string('report_to');
                 $table->decimal('salary', 10, 2);
                 $table->enum('employee_status', ['active', 'on-leave', 'terminated']);
                 $table->string('bank_name')->nullable();
@@ -40,6 +47,21 @@ return new class extends Migration
                 $table->string('ifsc_code')->nullable();
                 $table->string('emergency_contact')->nullable();
                 $table->string('password');
+                $table->string('image');
+                $table->string('blood_group');
+                $table->string('parent_bld_group');
+                $table->string('nationality');
+                $table->string('religion');
+                $table->enum('marital_status', ['married','unmarried']);
+                $table->string('spouse');
+                $table->enum('physically_challenge', ['yes','no']);
+                $table->enum('inter_emp',['yes','no']);
+                $table->string('job_location');
+                $table->string('education');
+                $table->string('experince');
+                $table->string('pan_no');
+                $table->string('adhar_no');
+                $table->string('pf_no');
                 $table->timestamps();
 
         });
@@ -66,7 +88,7 @@ return new class extends Migration
     BEGIN
         DECLARE next_id INT;
         SET next_id = (SELECT IFNULL(MAX(SUBSTRING_INDEX(emp_id, "-", -1) + 1), 1) FROM employee_details);
-        SET NEW.emp_id = CONCAT("payg-", LPAD(next_id, 4, "0"));
+        SET NEW.emp_id = CONCAT("PAYG-", LPAD(next_id, 4, "0"));
     END
 ');
 
--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -9,11 +9,16 @@
     <title>Grey HR Portal</title>
     @vite(['resources/css/app.css','resources/js/app.js'])
     <script src="{{ mix('js/app.js') }}"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('js/app.js') }}" defer data-turbolinks-track="reload"></script>
+    <!-- Include Alpine.js -->
+<script src="https://cdn.jsdelivr.net/npm/alpinejs@2.x.x/dist/alpine.js" defer></script>
+
     <script src="https://cdn.jsdelivr.net/gh/livewire/sortable@v0.x.x/dist/livewire-sortable.js"></script>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <script src="{{ asset('vendor/livewire/livewire.js') }}"></script>
