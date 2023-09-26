0bc628867b5cb0e673a6a474f7d30a96997f12c9
Asapu Sri Kumar Manikanta <manikanta.asapu@paygdigitals.com>
2023-09-26 15:31:12 +0530

    Commmon NavBar

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</head>
+@livewireStyles
+<body>
+@livewire('navbar')
+    <div id="app">
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+    @livewireScripts
+</body>
+</html>
