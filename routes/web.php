0df4aa961797ced9052eead01dfd30a05e62aa55
sssreddys <saragada566@gmail.com>
2023-10-04 16:22:27 +0530

    route changes

--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,6 @@ Route::get('/ProfileInfo', ProfileInfo::class)->name('profile.info');
 Route::get('/Feeds', Feeds::class);
 Route::get('/PeoplesList', PeopleLists::class);
 Route::get('/HelpDesk', HelpDesk::class);
-
-    Route::get('/emplogin', EmpLogin::class)->name('emplogin');
-    Route::get('/', Home::class)->name('home');
-    Route::get('/ProfileInfo', ProfileInfo::class)->name('profile.info');
-    Route::get('/Feeds', Feeds::class);
-    Route::get('/review', Review::class)->name('review');
-    Route::get('/task', Task::class)->name('task');
+Route::get('/review', Review::class)->name('review');
+Route::get('/task', Task::class)->name('task');
 
