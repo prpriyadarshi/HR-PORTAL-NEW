624af443713d778d12560b27f15c8b2916106941
Asapu sri kumar manikanta <122153339+Kumarmani234@users.noreply.github.com>
2023-10-06 17:55:31 +0530

    Update 2023_09_26_090006_create_employee_details_table.php

--- database/migrations/2023_09_26_090006_create_employee_details_table.php
+++ database/migrations/2023_09_26_090006_create_employee_details_table.php
@@ -10,6 +10,7 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
+     */
     public function up(): void
     {
         Schema::create('employee_details', function (Blueprint $table) {
