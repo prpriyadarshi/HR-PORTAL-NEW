e06a1b0e17d4b5a7e6eaaed2a39e2011e78d3122
sssreddys <saragada566@gmail.com>
2023-10-15 21:27:18 +0530

    changes

--- app/Livewire/SalaryRevisions.php
+++ app/Livewire/SalaryRevisions.php
@@ -1,13 +1,15 @@
 <?php
 
 namespace App\Livewire;
-
 use Livewire\Component;
 
 class SalaryRevision extends Component
 {
+    public $salaryRevision;
     public function render()
     {
+        $this->salaryRevision = SalaryRevision::where('emp_id',auth()->guard('emp')->user()->emp_id)->get();
+         dd($this->salaryRevision);
         return view('livewire.salary-revision');
     }
 }
