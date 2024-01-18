7d6a10268f286518acff2183f09c9b4ba656b8f3
Kumarmani234 <kumarmanikanta376@gmail.com>
2024-01-18 16:34:02 +0530

    New Changes

---
+++ resources/views/livewire/doc-forms.blade.php
@@ -0,0 +1,7 @@
+<div>
+    <button style="background-color: rgb(2,17,79);border:none;border-radius:5px"><a href="/document" style="text-decoration: none;color:white;">Back</a></button>
+    <div class="row m-3 d-flex flex-row">
+        <div style="color: rgb(2,17,79)"> Forms</div>
+        <hr style="width: 40px;text-align:start;border:2px solid rgb(2,17,79);color:rgb(2,17,79);margin-top:5px;border-radius:5px">
+    </div>
+</div>
